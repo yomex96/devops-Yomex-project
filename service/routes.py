ad54e6c18b8932206cc0a48a3f4d7a030540f34c
Abayomi Robert Onawole <142827967+yomex96@users.noreply.github.com>
2025-09-25 15:34:25 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -59,6 +59,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
